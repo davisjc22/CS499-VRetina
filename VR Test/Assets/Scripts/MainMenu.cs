343998daccd7dbb5cfdbe5089fb8c66b643483cf
davisjc22 <davisjc22@gmail.com>
2020-03-03 12:35:57 -0500

    New Project

---
+++ VR Test/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{   
+    void Start()
+    {
+        //StartCoroutine(LoadDevice(""));
+    }
+
+    public void LoadVR() //Loads the VR mode of the app
+    {
+        SceneManager.LoadScene("VRDemo");
+    }
+
+    public void LoadAR() //Loads the AR mode of the app
+    {
+        SceneManager.LoadScene("ARDemo");
+    }
+
+}
