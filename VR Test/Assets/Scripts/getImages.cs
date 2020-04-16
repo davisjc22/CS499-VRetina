fc577327f97c4c5f6f74605ff9955b2aea2a58a2
Hangjo <jthang98@gmail.com>
2020-04-15 21:07:28 -0400

    New image selection menu with local image folder
    -Uses custom image selector instead of native gallery image selector.
    -Now looks at images inside application folder instead of the phone's gallery.
    -Still needs improvements for different screen sizes and performance could be improved.

---
+++ VR Test/Assets/Scripts/getImages.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System;
+using System.Linq;
+using UnityEngine.UI;
+
+public class getImages : MonoBehaviour
+{
+    Texture2D[] textures = null;
+    public GameObject buttonholder;
+    public GameObject button;
+
+    private bool loaded = false;
+
+    private void Start()
+    {
+        StartCoroutine(findImages());
+    }
+
+    //public void images()
+    //{
+    //    if (!loaded)
+    //    {
+    //        loaded = true;  
+    //        StartCoroutine(findImages());
+    //    }
+    //}
+
+    IEnumerator findImages()
+    {
+        //Search for files
+        DirectoryInfo dir = new DirectoryInfo(Application.persistentDataPath + "/retinaImages/");
+        string[] extensions = new[] { ".jpg", ".JPG", ".jpeg", ".JPEG", ".png", ".PNG", ".ogg", ".OGG" };
+        FileInfo[] info = dir.GetFiles().Where(f => extensions.Contains(f.Extension.ToLower())).ToArray();
+
+        //yield return info;
+
+        for (int i = 0; i < info.Length; i++)
+        {
+            //GameObject newButton = Instantiate(button) as GameObject;
+            //yield return newButton;
+            //newButton.transform.SetParent(buttonholder.transform, false);
+            //newButton.transform.GetChild(0).GetComponent<Text>().text = Path.GetFileName(info[i].FullName);
+
+            byte[] bytes = File.ReadAllBytes(info[i].FullName);
+            //yield return bytes;
+
+            Texture2D texture = new Texture2D(2, 2);
+            yield return texture.LoadImage(bytes);
+
+            GameObject newButton = Instantiate(button) as GameObject;
+            newButton.transform.SetParent(buttonholder.transform, false);
+            newButton.GetComponent<RawImage>().texture = texture;
+            newButton.GetComponent<changeImage>().texture = texture;
+
+            //GameObject newButton = Instantiate(button) as GameObject;
+            //newButton.transform.SetParent(buttonholder.transform, false);
+            //newButton.GetComponent<Image>().sprite = Sprite.Create(texture, new Rect(0.0f, 0.0f, texture.width, texture.height), new Vector2(1f, 1f), 100.0f);
+            //newButton.GetComponent<Image>().SetNativeSize();
+            //newButton.GetComponent<changeImage>().texture = texture;
+            //yield return null;
+        }
+    }
+}
---
+++ VR Test/Assets/Scripts/openImageSelection.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class openImageSelection : MonoBehaviour
+{
+    public GameObject Scrollview;
+
+    public void openMenu()
+    {
+        Scrollview.gameObject.SetActive(true);
+    }
+
+    public void closeMenu()
+    {
+        Scrollview.gameObject.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            if (Input.GetKey(KeyCode.Escape))
+            {
+                closeMenu();
+            }
+        }
+    }
+}
