1095afb9a6fb01636c6d612933f0d0e16768de36
JarrenTay <jarrentayusa@gmail.com>
2020-04-18 00:49:58 -0400

    Pulls labels from db and creates them

--- VR Test/Assets/Scripts/GetImageScript.cs
+++ VR Test/Assets/Scripts/GetImageScript.cs
@@ -4,32 +4,106 @@ using System.Collections.Generic;
 using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Networking;
 
 [Serializable]
 public class RetinaImage
 {
-    public int id = -1;
-    public string name = "";
-    public int xSize = 0;
-    public int ySize = 0;
-    public string filetype = "";
-    public bool official = false;
-    public string uploaded = "";
-    public string imgBase64 = "";
+    public int id;
+    public string name;
+    public int xSize;
+    public int ySize;
+    public string filetype;
+    public bool official;
+    public string uploaded;
+    public string image;
+    public List<Label> labels;
+
+    public RetinaImage()
+    {
+        id = -1;
+        name = "";
+        xSize = 0;
+        ySize = 0;
+        filetype = "";
+        official = false;
+        uploaded = "";
+        image = "";
+    }
 }
 
-/*
-public class RetinaImage : MonoBehaviour
+[Serializable]
+public class Label
 {
-    public int id = -1;
-    public string name = "";
-    public int xSize = 0;
-    public int ySize = 0;
-    public string filetype = "";
-    public bool official = false;
-    public string uploaded = "";
-    public string imgBase64 = "";
+    public int labelId;
+    public string labelName;
+    public List<Mesh> meshes;
+}
+
+[Serializable]
+public class Mesh
+{
+    public int meshId;
+    public List<Vertex> vertices;
+    public List<Triangle> triangles;
+
+    public Mesh(Mesh inMesh)
+    {
+        Debug.Log(inMesh);
+        Debug.Log(inMesh.vertices[0].x);
+        Debug.Log(inMesh.meshId);
+        meshId = inMesh.meshId;
+    }
+}
+
+[Serializable]
+public class Vertex
+{
+    public int vertexNum;
+    public float x;
+    public float y;
+    public float z;
+}
+
+[Serializable]
+public class Triangle
+{
+    public int triangleNum;
+    public int a;
+    public int b;
+    public int c;
+}
+
+public class GetImageScript : MonoBehaviour
+{
+    private Renderer rend;
+    private Mesh mesh;
+    public RetinaImage retinaImage;
+    private bool notLoaded;
+    public int imageId = 4;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        retinaImage = new RetinaImage();
+        //StartCoroutine(GetImage(imageId, retinaImage));
+        notLoaded = true;
+        StartCoroutine(LoadImage(imageId));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (notLoaded && retinaImage.id != -1)
+        {
+            Debug.Log("Applied Image.");
+            notLoaded = false;
+            ApplyImage(rend);
+            CreateLabels();
+        }
+    }
 
     public IEnumerator GetImage(int id)
     {
@@ -48,236 +122,74 @@ public class RetinaImage : MonoBehaviour
             }
             else
             {
-                Debug.Log("request complete!");
+                Debug.Log("Webserver request complete");
 
                 string phrase = www.downloadHandler.text;
-                string[] words = phrase.Split(',');
 
-                foreach (var word in words)
-                {
-                    string[] tokens = word.Split('"');
-                    switch (tokens[1])
-                    {
-                        case "id":
-                            this.id = Convert.ToInt32(tokens[3]);
-                            break;
-                        case "name":
-                            this.name = tokens[3];
-                            break;
-                        case "xSize":
-                            this.xSize = Convert.ToInt32(tokens[3]);
-                            break;
-                        case "ySize":
-                            this.ySize = Convert.ToInt32(tokens[3]);
-                            break;
-                        case "filetype":
-                            this.filetype = tokens[3];
-                            break;
-                        case "official":
-                            if (tokens[3] == "1")
-                            {
-                                this.official = true;
-                            }
-                            else
-                            {
-                                this.official = false;
-                            }
-                            break;
-                        case "uploaded":
-                            this.uploaded = tokens[3];
-                            break;
-                        case "image":
-                            this.imgBase64 = tokens[3];
-                            break;
-                    }
-                }
-                Debug.Log("Retina Image object updated.");
+                retinaImage = JsonUtility.FromJson<RetinaImage>(phrase);
+
+                Debug.Log("Retina image object updated.");
             }
+        }
+    }
+
+    public void CreateLabels()
+    {
+        GameObject labels = new GameObject("Labels");
+        labels.transform.SetParent(this.transform);
+        labels.transform.localPosition = new Vector3(0, 0, 0);
+        foreach (Label label in retinaImage.labels)
+        {
+            GameObject componentLabel = new GameObject(label.labelId.ToString());
+            Text labelName = componentLabel.AddComponent<Text>();
+            labelName.text = label.labelName;
+            componentLabel.GetComponent<Text>().enabled = false;
+
+            foreach (Mesh mesh in label.meshes)
+            {
+                GameObject componentMesh = new GameObject(mesh.meshId.ToString());
+                MeshFilter mf = componentMesh.AddComponent<MeshFilter>();
+                MeshCollider mc = componentMesh.AddComponent<MeshCollider>();
+
+                UnityEngine.Mesh genMesh = new UnityEngine.Mesh();
+
+                Vector3[] newVertices = new Vector3[mesh.vertices.Count];
+                int[] newTriangles = new int[mesh.triangles.Count * 3];
+
+                foreach (Vertex vertex in mesh.vertices)
+                {
+                    newVertices[vertex.vertexNum] = new Vector3(vertex.x, vertex.y, vertex.z);
+                }
+
+                foreach (Triangle triangle in mesh.triangles)
+                {
+                    newTriangles[triangle.triangleNum * 3] = triangle.a;
+                    newTriangles[(triangle.triangleNum * 3) + 1] = triangle.b;
+                    newTriangles[(triangle.triangleNum * 3) + 2] = triangle.c;
+                }
+                genMesh.vertices = newVertices;
+                genMesh.triangles = newTriangles;
+
+                mf.mesh = genMesh;
+                mc.sharedMesh = genMesh;
+                componentMesh.transform.SetParent(componentLabel.transform);
+                componentMesh.transform.localPosition = new Vector3(0, 0, 0);
+            }
+            componentLabel.transform.SetParent(labels.transform);
+            componentLabel.transform.localPosition = new Vector3(0, 0, 0);
         }
     }
 
     public void ApplyImage(Renderer rend)
     {
-        byte[] imageBytes = Convert.FromBase64String(this.imgBase64);
-        Texture2D tex = new Texture2D(this.xSize, this.ySize);
-        tex.LoadImage(imageBytes);
-        rend.material.SetTexture("_MainTex", tex);
-        Debug.Log("Updated Texture.");
-    }
-
-    public void SaveToFile()
-    {
-        string destinationFolder = Application.persistentDataPath + "/retinaImages/";
-        string destination = destinationFolder + this.id.ToString() + ".dat";
-        FileStream file;
-
-        if (!Directory.Exists(destinationFolder))
-        {
-            Directory.CreateDirectory(destinationFolder);
-        }
-
-        if (File.Exists(destination))
-        {
-            file = File.OpenWrite(destination);
-        }
-        else
-        {
-            file = File.Create(destination);
-        }
-
-        BinaryFormatter bf = new BinaryFormatter();
-        RetinaImageSerializable retinaImageSerializable = new RetinaImageSerializable(this);
-        bf.Serialize(file, retinaImageSerializable);
-        file.Close();
-    }
-
-    public IEnumerator LoadImage(int id)
-    {
-        Debug.Log("Load Image Called");
-        string destinationFolder = Application.persistentDataPath + "/retinaImages/";
-        string destination = destinationFolder + id.ToString() + ".dat";
-        Debug.Log(destination);
-        FileStream file;
-
-        if (!Directory.Exists(destinationFolder))
-        {
-            Directory.CreateDirectory(destinationFolder);
-        }
-
-        Debug.Log("Checking if file exists");
-        if (File.Exists(destination))
-        {
-            Debug.Log("Loaded from file");
-            file = File.OpenRead(destination);
-            BinaryFormatter bf = new BinaryFormatter();
-            RetinaImageSerializable tempRetinaImage = (RetinaImageSerializable)bf.Deserialize(file);
-            this.id = tempRetinaImage.id;
-            this.name = tempRetinaImage.name;
-            this.xSize = tempRetinaImage.xSize;
-            this.ySize = tempRetinaImage.ySize;
-            this.filetype = tempRetinaImage.filetype;
-            this.official = tempRetinaImage.official;
-            this.uploaded = tempRetinaImage.uploaded;
-            this.imgBase64 = tempRetinaImage.imgBase64;
-            file.Close();
-        }
-        else
-        {
-            Debug.Log("Downloaded from webserver");
-            StartCoroutine(this.GetImage(id));
-            while (this.id == -1)
-                yield return new WaitForSeconds(0.1f);
-            this.SaveToFile();
-            Debug.Log("Saving to File");
-        }
-    }
-}*/
-
-public class GetImageScript : MonoBehaviour
-{
-    private Renderer rend;
-    public RetinaImage retinaImage;
-    private bool notLoaded;
-    public int imageId = 4;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        rend = GetComponent<Renderer>();
-        retinaImage = new RetinaImage();
-        //StartCoroutine(GetImage(imageId, retinaImage));
-        notLoaded = true;
-        StartCoroutine(LoadImage(retinaImage, imageId));
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        Debug.Log(retinaImage.name);
-        if (notLoaded && retinaImage.id != -1)
-        {
-            Debug.Log("Applied.");
-            notLoaded = false;
-            ApplyImage(retinaImage, rend);
-        }
-        
-    }
-
-    public IEnumerator GetImage(RetinaImage retinaImage, int id)
-    {
-        WWWForm form = new WWWForm();
-
-        form.AddField("id", id);
-        form.AddField("clientType", "app");
-
-        using (UnityWebRequest www = UnityWebRequest.Post("http://ec2-13-58-160-235.us-east-2.compute.amazonaws.com/request.php", form))
-        {
-            yield return www.SendWebRequest();
-
-            if (www.isNetworkError || www.isHttpError)
-            {
-                Debug.Log(www.error);
-            }
-            else
-            {
-                Debug.Log("request complete!");
-
-                string phrase = www.downloadHandler.text;
-                string[] words = phrase.Split(',');
-
-                foreach (var word in words)
-                {
-                    string[] tokens = word.Split('"');
-                    switch (tokens[1])
-                    {
-                        case "id":
-                            retinaImage.id = Convert.ToInt32(tokens[3]);
-                            break;
-                        case "name":
-                            retinaImage.name = tokens[3];
-                            break;
-                        case "xSize":
-                            retinaImage.xSize = Convert.ToInt32(tokens[3]);
-                            break;
-                        case "ySize":
-                            retinaImage.ySize = Convert.ToInt32(tokens[3]);
-                            break;
-                        case "filetype":
-                            retinaImage.filetype = tokens[3];
-                            break;
-                        case "official":
-                            if (tokens[3] == "1")
-                            {
-                                retinaImage.official = true;
-                            }
-                            else
-                            {
-                                retinaImage.official = false;
-                            }
-                            break;
-                        case "uploaded":
-                            retinaImage.uploaded = tokens[3];
-                            break;
-                        case "image":
-                            retinaImage.imgBase64 = tokens[3];
-                            break;
-                    }
-                }
-                Debug.Log("Retina Image object updated.");
-            }
-        }
-    }
-
-    public void ApplyImage(RetinaImage retinaImage, Renderer rend)
-    {
-        byte[] imageBytes = Convert.FromBase64String(retinaImage.imgBase64);
+        byte[] imageBytes = Convert.FromBase64String(retinaImage.image);
         Texture2D tex = new Texture2D(retinaImage.xSize, retinaImage.ySize);
         tex.LoadImage(imageBytes);
         rend.material.SetTexture("_MainTex", tex);
-        Debug.Log("Updated Texture.");
+        Debug.Log("Updated Texture on model.");
     }
 
-    public void SaveToFile(RetinaImage retinaImage)
+    public void SaveToFile()
     {
         string destinationFolder = Application.persistentDataPath + "/retinaImages/";
         string destination = destinationFolder + retinaImage.id.ToString() + ".dat";
@@ -302,9 +214,8 @@ public class GetImageScript : MonoBehaviour
         file.Close();
     }
 
-    public IEnumerator LoadImage(RetinaImage retinaImage, int id)
+    public IEnumerator LoadImage(int id)
     {
-        Debug.Log("Load Image Called");
         string destinationFolder = Application.persistentDataPath + "/retinaImages/";
         string destination = destinationFolder + id.ToString() + ".dat";
         Debug.Log(destination);
@@ -315,7 +226,6 @@ public class GetImageScript : MonoBehaviour
             Directory.CreateDirectory(destinationFolder);
         }
 
-        Debug.Log("Checking if file exists");
         if (File.Exists(destination))
         {
             Debug.Log("Loaded from file");
@@ -329,16 +239,17 @@ public class GetImageScript : MonoBehaviour
             retinaImage.filetype = tempRetinaImage.filetype;
             retinaImage.official = tempRetinaImage.official;
             retinaImage.uploaded = tempRetinaImage.uploaded;
-            retinaImage.imgBase64 = tempRetinaImage.imgBase64;
+            retinaImage.image = tempRetinaImage.image;
+            retinaImage.labels = tempRetinaImage.labels;
             file.Close();
         }
         else
         {
-            Debug.Log("Downloaded from webserver");
-            StartCoroutine(GetImage(retinaImage, id));
+            Debug.Log("Downloading from webserver");
+            StartCoroutine(GetImage(id));
             while (retinaImage.id == -1)
                 yield return new WaitForSeconds(0.1f);
-            SaveToFile(retinaImage);
+            SaveToFile();
             Debug.Log("Saving to File");
         }
     }
